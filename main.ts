4228a6adf1d5367f7ae0811f304c27d35e5c89ec
Ondřej Tůma <ondrej.tuma.024@pslib.cz>
2024-12-16 10:09:19 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,12 @@
-//basic.forever(function() {
-//    if (input.buttonIsPressed(Button.A)) {
-//        music.playTone(440, 100)
-//    }
-//    if (input.buttonIsPressed(Button.A)) {
-//        while (input.buttonIsPressed(Button.A)) {
-//            break
-//        }
-//    }
-//})
+let buttonPressed = input.buttonIsPressed(Button.A)
 
-let x = input.buttonIsPressed(Button.A)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) && !buttonPressed) {
+        music.playTone(440, 100)
 
-basic.forever(function() {
-    if (x) {
-        do {
-            music.playTone(440, 100)
-            x = false
-        } while (x)
+        buttonPressed = true
+    }
+    if (!input.buttonIsPressed(Button.A)) {
+        buttonPressed = false
     }
 })
